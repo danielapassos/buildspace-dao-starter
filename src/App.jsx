9cee8ef4a994b65b2cc8946c7be10a9522610212
Daniela Passos <moitidani@gmail.com>
2022-09-11 21:30:14 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to Thouxs</h1>
+        <h1>Welcome to Thoux</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
